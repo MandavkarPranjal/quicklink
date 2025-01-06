4ec180e036c57bcd9179b1bf3ac5e7b5388009b5
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-01-06 15:16:07 +0530

    add: orm category

---
+++ src/app/category/orm/orm-category.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { LinkPreview } from "@/components/ui/link-preview";
+
+interface Tool {
+    name: string;
+    description: string;
+    url: string;
+}
+
+export default function OrmCategory() {
+    const [tools, setTools] = useState<Tool[]>([]);
+
+    useEffect(() => {
+        fetch("/docs/tools.json")
+            .then((response) => response.json())
+            .then((data) => {
+                const databaseCategory = data.categories.find(
+                    (category: { name: string }) => category.name.toLowerCase() === "orm"
+                );
+                setTools(databaseCategory ? databaseCategory.tools : []);
+            })
+            .catch((error) => console.error("Failed to load tools:", error));
+    }, []);
+
+    return (
+        <main className="flex flex-grow flex-col items-center justify-center text-center">
+            <div className="text-5xl font-semibold">
+                ORM
+            </div>
+            <div className="mt-4"></div>
+            <div className="">
+                {tools.map((tool) => (
+                    <div key={tool.name}>
+                        <LinkPreview
+                            url={tool.url}
+                        >
+                            <Link
+                                href={tool.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <span className="text-blue-500 hover:underline" >
+                                    {tool.name}
+                                </span>
+                                - <span className="text-gray-500 hover:text-white/60">{tool.description}</span>
+                            </Link>
+                        </LinkPreview>
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
+}
---
+++ src/app/category/orm/page.tsx
@@ -0,0 +1,10 @@
+import OrmCategory from "./orm-category";
+
+export const metadata = {
+    title: "ORMs - QuickLinks",
+    description: "Best ORMs that you can find",
+};
+
+export default function SVGToolPage() {
+    return <OrmCategory />;
+}
