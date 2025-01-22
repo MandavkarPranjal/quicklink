832d8a071f2d2d7b6fc2280d0d25ac8737a094ad
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-01-22 22:20:16 +0530

    add: design inspiration

---
+++ src/app/category/design-inspiration/page.tsx
@@ -0,0 +1,10 @@
+import InspirationCategory from "./inspiration-category";
+
+export const metadata = {
+    title: "Cool Tools - QuickLinks",
+    description: "Tools that will make your life easier",
+};
+
+export default function SVGToolPage() {
+    return <InspirationCategory />;
+}
